87391d5a65c4271875ff03d5f81d9713f21483fe
p-lab1909 <60678270+p-lab1909@users.noreply.github.com>
2022-04-11 04:00:31 +0100

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -1,5 +1,12 @@
 import importlib
+import os
 import re
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
 from typing import Optional, List
 
 from telegram import Message, Chat, Update, Bot, User
